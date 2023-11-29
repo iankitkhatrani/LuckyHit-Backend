63589215de11e31e1e616ae04f157a8e1d25bc80
ankit <iankitkhatrani@gmail.com>
2023-11-29 17:08:40 +0530

    resolve
    fix number resolve

--- src/aviator/gameStart.js
+++ src/aviator/gameStart.js
@@ -121,23 +121,29 @@ module.exports.startAviator = async (tbId) => {
 }       
 
 module.exports.generateNumber=async(minRange,maxRange)=>{
-    // Generate a random decimal number between 0 (inclusive) and 1 (exclusive)
-    const randomDecimal = Math.random().toFixed(2);
-    console.log('Random Decimal:', randomDecimal);
 
-    // Generate a random whole number between a specified range (min and max)
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    return new Promise((resolve) => {
 
-    const randomWholeNumber = getRandomInt(minRange, maxRange);
-    console.log('Random Whole Number:randomWholeNumber ', randomWholeNumber);
 
-    console.log('Random Whole Number:', randomWholeNumber+parseFloat(randomDecimal));
+        // Generate a random decimal number between 0 (inclusive) and 1 (exclusive)
+        const randomDecimal = Math.random().toFixed(2);
+        console.log('Random Decimal:', randomDecimal);
 
-    return randomWholeNumber+parseFloat(randomDecimal)
+        // Generate a random whole number between a specified range (min and max)
+        function getRandomInt(min, max) {
+            min = Math.ceil(min);
+            max = Math.floor(max);
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+        const randomWholeNumber = getRandomInt(minRange, maxRange);
+        console.log('Random Whole Number:randomWholeNumber ', randomWholeNumber);
+
+        console.log('Random Whole Number:', randomWholeNumber+parseFloat(randomDecimal));
+
+        return resolve(randomWholeNumber+parseFloat(randomDecimal))
+    })
+
 }
 
 
