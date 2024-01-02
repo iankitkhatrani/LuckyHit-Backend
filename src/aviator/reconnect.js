e8b338514647e9323e39db6c6468ad5c380a206e
ankit <iankitkhatrani@gmail.com>
2024-01-02 15:16:44 +0530

    rendomNumber
    rendomNumber

--- src/aviator/reconnect.js
+++ src/aviator/reconnect.js
@@ -73,7 +73,7 @@ module.exports.
                     tableid: tabInfo._id,
                     gamePlayType: tabInfo.gamePlayType,
                     sceneName: CONST.GAMEPLAY,
-                    rendomNumber: tabInfo.Number,
+                    rendomNumber: tabInfo.rendomNumber,
                 };
 
                 if (tabInfo.gameState === "StartEviator") {
