e4f2fde13451541b10ac788fac46dd7a1a6c7619
ankit <iankitkhatrani@gmail.com>
2024-02-05 12:46:03 +0530

    chanage table name

--- database/mongoDbConnection.js
+++ database/mongoDbConnection.js
@@ -6,7 +6,11 @@ const logger = require('../logger');
 //Set up default mongoose connection
 const mongoDB = config.DATABASE;
 
-mongoose.connect(mongoDB, {
+mongoose.connect(mongoDB,
+  {
+    dbName: 'lukcyhitssb',
+  },
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
